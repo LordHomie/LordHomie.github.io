4ce35f9a1d799261199699a7112d6ecbc1d45460
LordHomie <hamouda99@windowslive.com>
2022-03-01 21:40:48 +0300

    commit updates

--- src/components/Contact/ContactComponents/RightContentComponent.js
+++ src/components/Contact/ContactComponents/RightContentComponent.js
@@ -1,10 +1,102 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useReducer, useEffect, useRef } from "react";
 import Button from "../../UI/Button";
+import Input from "../../UI/Input";
+import TextArea from "../../UI/TextArea";
 import FormSent from "./FormSent";
 
 import classes from "./RightContentComponent.module.css";
 
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const subjectReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 0 };
+  }
+  return { value: "", isValid: false };
+};
+
+const nameReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 0 };
+  }
+  return { value: "", isValid: false };
+};
+
+const textAreaReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 0 };
+  }
+  return { value: "", isValid: false };
+};
+
 const RightContentComponent = () => {
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [subjectState, dispatchSubject] = useReducer(subjectReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [nameState, dispatchName] = useReducer(nameReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [textAreaState, dispatchTextArea] = useReducer(textAreaReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: subjectIsValid } = subjectState;
+  const { isValid: nameIsValid } = nameState;
+  const { isValid: textAreaIsValid } = textAreaState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        emailIsValid && subjectIsValid && nameIsValid && textAreaIsValid
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, subjectIsValid, nameIsValid, textAreaIsValid]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const subjectChangeHandler = (event) => {
+    dispatchSubject({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const nameChangeHandler = (event) => {
+    dispatchName({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const textAreaChangeHandler = (event) => {
+    dispatchTextArea({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const resetInputs = () => {
+    dispatchEmail({ type: "USER_INPUT", val: "" });
+    dispatchSubject({ type: "USER_INPUT", val: "" });
+    dispatchName({ type: "USER_INPUT", val: "" });
+    dispatchTextArea({ type: "USER_INPUT", val: "" });
+  };
+
   const [isformHidden, setIsFormHidden] = useState(false);
   const [isSentIconHidden, setIsSentIconHidden] = useState(true);
 
@@ -18,9 +110,25 @@ const RightContentComponent = () => {
     setIsSentIconHidden(true);
   };
 
+  const nameInputRef = useRef();
+  const subjectInputRef = useRef();
+  const emailInputRef = useRef();
+  const textAreaInputRef = useRef();
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    hideForm();
+    if (formIsValid) {
+      hideForm();
+      resetInputs();
+    } else if (!nameIsValid) {
+      nameInputRef.current.focus();
+    } else if (!subjectIsValid) {
+      subjectInputRef.current.focus();
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      textAreaInputRef.current.focus();
+    }
   };
 
   return (
@@ -32,14 +140,35 @@ const RightContentComponent = () => {
       >
         <h4 className={classes["form-title"]}>Send me a message</h4>
         <form onSubmit={formSubmitHandler}>
-          <input type="text" placeholder="Name..." />
-          <input type="text" placeholder="Subject..." />
-          <input type="email" placeholder="Email..." />
-          <textarea
+          <Input
+            ref={nameInputRef}
+            type="text"
+            placeholder="Name..."
+            value={nameState.value}
+            onChange={nameChangeHandler}
+          />
+          <Input
+            ref={subjectInputRef}
+            type="text"
+            placeholder="Subject..."
+            value={subjectState.value}
+            onChange={subjectChangeHandler}
+          />
+          <Input
+            ref={emailInputRef}
+            type="email"
+            placeholder="Email..."
+            value={emailState.value}
+            onChange={emailChangeHandler}
+          />
+          <TextArea
+            ref={textAreaInputRef}
             rows="4"
             cols="33"
             placeholder="Tell me about your project ideas..."
-          ></textarea>
+            value={textAreaState.value}
+            onChange={textAreaChangeHandler}
+          />
           <Button className={classes["contact-button"]}>Send message</Button>
         </form>
       </section>
---
+++ src/components/UI/Input.js
@@ -0,0 +1,29 @@
+import React, { Fragment, useRef, useImperativeHandle } from "react";
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
+  return (
+    <Fragment>
+      <input
+        ref={inputRef}
+        type={props.type}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </Fragment>
+  );
+});
+
+export default Input;
